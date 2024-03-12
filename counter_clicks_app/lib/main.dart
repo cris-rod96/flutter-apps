09dd3d7f553b9c1818226fcfca7ba05ff24fa0b8
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-03-11 21:54:00 -0500

    Counter Clicks App Completed

--- counter_clicks_app/lib/main.dart
+++ counter_clicks_app/lib/main.dart
@@ -1,14 +1,76 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text("Hi world"),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Counter Clicks"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '$counter',
+                  style: const TextStyle(fontSize: 120),
+                ),
+                const Text(
+                  "clicks",
+                  style: TextStyle(fontSize: 25),
+                )
+              ],
+            ),
+          ),
+          floatingActionButton: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    counter = 0;
+                  });
+                },
+                child: const Icon(Icons.restart_alt_rounded),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    counter++;
+                  });
+                },
+                child: const Icon(Icons.plus_one_rounded),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  if (counter == 0) return;
+                  setState(() {
+                    counter--;
+                  });
+                },
+                child: const Icon(Icons.exposure_minus_1_rounded),
+              ),
+            ],
+          )),
     );
   }
 }
